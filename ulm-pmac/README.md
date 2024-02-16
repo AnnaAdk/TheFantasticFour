63a3f52c01741dd2b5f860576169a095367e0aee
Olivia Howard <106042786+OliRosHow@users.noreply.github.com>
2024-02-15 18:33:41 -0600

    adding starting build

---
+++ ulm-pmac/README.md
@@ -0,0 +1,6 @@
+# ulm-pmac capstone project
+
+## to start development:
+    npm install
+    npm run dev
+
---
+++ ulm-pmac/src/App.jsx
@@ -0,0 +1,12 @@
+import ulmLogo from "./assets/ulm-academic-logo-circle.png";
+
+function App() {
+  return (
+    <>
+    <img src={ulmLogo}/>
+    </>
+  )
+}
+
+export default App
+

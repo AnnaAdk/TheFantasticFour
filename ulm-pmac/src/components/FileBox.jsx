c77606fc047d4b6e8e3c7cd9388671f6f9b62831
Olivia Howard <106042786+OliRosHow@users.noreply.github.com>
2024-02-17 13:33:27 -0600

    added feedback visuals for file box

--- ulm-pmac/src/components/FileBox.jsx
+++ ulm-pmac/src/components/FileBox.jsx
@@ -1,14 +1,53 @@
-import React,  { useCallback, useMemo} from 'react';
+import React,  { useCallback, useMemo, useEffect, useState} from 'react';
 import { useDropzone } from 'react-dropzone';
+import filePng from '../assets/file.png';
+
+const baseStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '1.5in',
+  borderWidth: 2,
+  borderRadius: 2,
+  borderStyle: 'solid',
+  backgroundColor: '#fafafa',
+  color: '#bdbdbd',
+  borderColor: '#840029',
+  transition: 'border .3s ease-in-out'
+};
+const activeStyle = {
+  borderColor: '#2196ff'
+};
 
 const acceptStyle = {
   borderColor: '#00ff00'
 };
 
+const rejectStyle = {
+  borderColor: '#ff0000'
+};
+
 function FileBox() {
+
+
   const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles);
+    setFiles(acceptedFiles.map(file => Object.assign(file, {
+      preview: URL.createObjectURL(file)
+    })));
   }, []);
+
+  const [files, setFiles] = useState([]);
+  
+  const filePreview = files.map(file => (
+      <img
+        key={file.name}
+        src={file.preview}
+        alt={file.name}
+        style={{position: '', left: '50%', top: '30%', height: '100px'}}
+      />
+
+  ));
+
 
   const {
     getRootProps, 
@@ -18,16 +57,36 @@ function FileBox() {
     isDragReject
     } = useDropzone({
         onDrop,
-        accept: "image/jpeg, image/png"
+        accept: 'image/jpeg, image/png, application/pdf'
       });
   
+  const style = useMemo(() => ({
+    ...baseStyle,
+    ...(isDragActive ? activeStyle : {}),
+    ...(isDragAccept ? acceptStyle : {}),
+    ...(isDragReject ? rejectStyle : {})
+  }), [
+    isDragActive,
+    isDragReject,
+    isDragAccept
+  ]);
 
+  useEffect(() => () => {
+    files.forEach(file => URL.revokeObjectURL(file.preview));
+  }, [files]);
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      <div className="FileBox">Drag and drop your files here.</div>
-    </div>
+    <>
+      <section className='FileBox' {...getRootProps({ style })}>
+        <input {...getInputProps()} />
+        <div>
+          {filePreview}
+        </div>
+        <div>
+          Drop your file here.
+        </div>
+      </section>
+    </>
   )
 }
 

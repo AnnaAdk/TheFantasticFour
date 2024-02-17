8e4efd1b2c4950b388a81596d34dd0bb74699ef0
Olivia Howard <106042786+OliRosHow@users.noreply.github.com>
2024-02-17 09:51:27 -0600

    added file submit code

--- ulm-pmac/src/components/App.jsx
+++ ulm-pmac/src/components/App.jsx
@@ -1,5 +1,6 @@
 import file from "../assets/file.png";
 import FileBox from "./FileBox";
+
 function App() {
   return (
     <>
--- ulm-pmac/src/components/FileBox.jsx
+++ ulm-pmac/src/components/FileBox.jsx
@@ -1,12 +1,27 @@
-import React,  { useCallback } from 'react';
+import React,  { useCallback, useMemo} from 'react';
 import { useDropzone } from 'react-dropzone';
+
+const acceptStyle = {
+  borderColor: '#00ff00'
+};
 
 function FileBox() {
   const onDrop = useCallback(acceptedFiles => {
     console.log(acceptedFiles);
   }, []);
 
-  const {getRootProps, getInputProps} = useDropzone({onDrop});
+  const {
+    getRootProps, 
+    getInputProps, 
+    isDragAccept, 
+    isDragActive, 
+    isDragReject
+    } = useDropzone({
+        onDrop,
+        accept: "image/jpeg, image/png"
+      });
+  
+
 
   return (
     <div {...getRootProps()}>
